3633cfc2900a06ee9862b446b4bb729dea15ef21
aurevec <aurelien@aurevec.be>
2017-03-31 23:16:05 +0200

    Add bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,5 @@ var ReactDOM = require('react-dom');
 var ListManager = require('./components/ListManager.jsx');
 
 ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
+ReactDOM.render(<ListManager title="ToDo" />, document.getElementById('todo'));
+ReactDOM.render(<ListManager title="Christmas" headingColor="#b31217" />, document.getElementById('christmas'));
